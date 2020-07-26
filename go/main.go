0dec648a477264fe75716ed1af57ef5da307d490
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-07-26 09:46:41 -0700

    format the go way

--- go/main.go
+++ go/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-"fmt"
-"github.com/julienschmidt/httprouter"
-"net/http"
-"log"
-"strconv"
-"encoding/json"
+	"encoding/json"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func fib() func() int {
 	a, b, c := 0, 1, 0
 	return func() int {
-	c, a, b = a, b, a+b
-	return c
+		c, a, b = a, b, a+b
+		return c
 	}
 }
 
@@ -23,15 +23,15 @@ func GetNumbers(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	maxFib, _ := strconv.Atoi(ps.ByName("number"))
 	response := make([]int, 0)
 	f := fib()
-		for i := 0; i < maxFib; i++ {
-			response = append(response, f())
-		}
-		jsonArray, _ := json.Marshal(response)
-		fmt.Fprint(w, string(jsonArray))
+	for i := 0; i < maxFib; i++ {
+		response = append(response, f())
+	}
+	jsonArray, _ := json.Marshal(response)
+	fmt.Fprint(w, string(jsonArray))
 }
 
 func main() {
-router := httprouter.New()
-router.GET("/api/fibonacci/:number", GetNumbers)
-log.Fatal(http.ListenAndServe(":8080", router))
+	router := httprouter.New()
+	router.GET("/api/fibonacci/:number", GetNumbers)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
